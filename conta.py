4485acf8e6a91ebe435e66f3f31c7919c74ad892
pires14 <joao.bernardo.pires@escola.pr.gov.br>
2026-05-25 11:29:17 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -3,29 +3,41 @@
 #``pass`` serve para mostrar que algo está vazio propositalmente para não gerar erro.
 #Class é em maiúsculo por padrão
 # init é para iniciar a função.
-# uma função para ser um método, precisa estar dentro de uma classe.
+# função: fora da classe
+# Método: dentro da classe
 
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite 
-        
-#Declaração do métodos(funções)
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite 
+    
+    #Ao colocar 2 __ , significa que não é para ser alterado diretamente. 
+    # Eles ficam privados para serem acessados pelos métodos.
+    #Declaração dos métodos(funções)
 
     def extrato(self):
-        print(f"Saldo: {self.saldo} do titular {self.titular}") 
+        print(f'Saldo: {self.__saldo} do titular {self.__titular}') 
 
     def depositar(self, valor):
         if(valor < 0):
             print(f'Não é possível depositar valores negativos')
         else:
-            self.saldo += valor #saldo = saldo + valor
+            self.__saldo += valor #saldo = saldo + valor
 
     def sacar(self, valor):
-        if(self.saldo < valor):
+        if(self.__saldo < valor):
             print(f'Não foi possível sacar este valor.')
         else: 
-            self.saldo -= valor
+            self.__saldo -= valor
 
+    def transferir(self, valor, conta_destino):
+        if(self.__saldo < valor) or (valor < 0):
+            print(f'Não é possível realizar a transferência.')
+        else:
+            self.sacar(valor)
+            conta_destino.depositar(valor)
+        
+    def inadimplentes(self, cliente):
+        pass
--- exec.py
+++ exec.py
@@ -1,13 +1,16 @@
 from conta import Conta
 
-conta = Conta(535, "Ricardo", 55.0, 1000.0)
-conta1 = Conta(555, "Luana", 100.0, 1000.0)
+conta1= Conta(535, "Ricardo", 55.0, 1000.0)
+conta2 = Conta(555, "Luana", 100.0, 1000.0)
 
-#Não é possível sacar se não tem o dinheiro necessário
+#Não apresenta erro:
+#conta.extrato()
+#Apresenta erro:
+#conta1.__saldo
 
-conta1.depositar(-10)
+conta1.transferir(10, conta2)
 conta1.extrato()
-
+conta2.extrato()
 
 #Ao printar, o PY mostra o endereço no computador onde está sendo salvo
 #Classes também recebem parâmetros
